f95bd97f43a9674f7c0e34e5075cfa292c308863
PRED-ONE\al5mo <al.5.montagni@gmail.com>
2024-12-27 04:45:18 +0100

    27 dec 24

--- projects/eser038/src/app/app.routes.ts
+++ projects/eser038/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DashboardComponent, pathMatch: 'full'},
+    {path: 'register', component: RegisterComponent, pathMatch: 'full'},
+    {path: 'login', component:  LoginComponent , pathMatch: 'full'}
+];
