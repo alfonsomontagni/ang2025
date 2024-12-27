d64d031e976c6bffb0486834e33ceb351f0ccf6a
PRED-ONE\al5mo <al.5.montagni@gmail.com>
2024-12-27 16:09:39 +0100

    jest

---
+++ setup-jest.ts
@@ -0,0 +1,5 @@
+// setup-jest.ts (nuovo metodo)
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+// Se serve configurare qualcosa, puoi passare un oggetto di configurazione
+setupZoneTestEnv();
