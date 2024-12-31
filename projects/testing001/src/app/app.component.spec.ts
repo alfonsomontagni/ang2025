e4f5ebd7b0f736226696a1ad791de27775038409
PRED-ONE\al5mo <al.5.montagni@gmail.com>
2024-12-31 09:03:52 +0100

    guards jest

--- projects/testing001/src/app/app.component.spec.ts
+++ projects/testing001/src/app/app.component.spec.ts
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-  /*
+ 
   it(`should have the 'testing001' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
@@ -27,5 +27,5 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, testing001');
-  });*/
+  }); 
 });
